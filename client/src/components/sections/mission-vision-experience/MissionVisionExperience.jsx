a2fecaec252562c825f48c9bdf16cc9d0ed2fe42
Polar <cristianga2002@ciencias.unam.mx>
2024-08-07 14:55:32 -0600

    Agregando mision-vision-experiencia

--- client/src/components/sections/mission-vision-experience/MissionVisionExperience.jsx
+++ client/src/components/sections/mission-vision-experience/MissionVisionExperience.jsx
@@ -4,7 +4,22 @@ import "./MissionVisionExperience.css";
 const MissionVisionExperience = () => {
     return (
         <section className="mission-vision-experience-container">
-            Aqui va la mision, vision y experiencia
+            <div>
+                <h1>Misión</h1>
+                <p>Brindar servicios de Consultoría Actuarial a las empresas de tal manera que les permita cubrir sus necesidades, minimizar riesgos y tener una adecuada toma de decisiones. Crear Valor y Diferencia.</p>
+            </div>
+            <div>
+                <h1>Visión</h1>
+                <p>Ser una empresa líder de Consultoría Actuarial por medio de productos innovadores, los cuales combinados con nuestra amplia experiencia permitan poner a disposición de nuestros clientes una perspectiva de negocios diferente y única, así como a sus empleados mejores prestaciones y condiciones laborales.</p>
+            </div>
+            <div>
+                <h1>Experiencia</h1>
+                <ul>
+                    <li>Estamos comprometidos a proporcionar una asesoría del más alto nivel de calidad, aportando experiencia práctica que hemos adquirido al participar en la asesoría de proyectos. Nuestra experiencia comprende instituciones financieras, Industria manufacturera, farmacéutica, Industria Automotriz, Comunicaciones, consumo, entre otras.</li>
+                    <li>Contamos con un grupo de expertos con una amplia experiencia profesional en asesoría en valuación, diseño, rediseño y comunicación de planes de pensiones y auditoria.</li>
+                    <li>Nuestro enfoque integral se orienta no solo al desarrollo de modelos actuariales, sino a la identificación, mitigación y cobertura de riesgos asociados a la administración de beneficios a empleados.</li>
+                </ul>
+            </div>
         </section>
     );
 };
