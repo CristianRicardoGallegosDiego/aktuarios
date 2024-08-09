6c1373b1a90db085e85e321d9dc7f733ef132af4
Polar <cristianga2002@ciencias.unam.mx>
2024-08-09 12:10:26 -0600

    Agregando esqueleto de ussgaap

--- client/src/pages/nif-d3/NifD3.jsx
+++ client/src/pages/nif-d3/NifD3.jsx
@@ -4,7 +4,54 @@ import "./NifD3.css";
 const NifD3 = () => {
     return (
         <section>
-
+            <h1>NIF D-3</h1>
+            <div>
+                <div>
+                    <h2>Antecedentes</h2>
+                    <div>
+                        <p>El 29 de agosto el Consejo Mexicano para la Investigación y Desarrollo de Normas de Información Financiera (CINIF) emitió la Norma de Información Financiera D-3 “Beneficios a los Empleados” (NIF D-3), la cual entró en vigor a partir del 1 de enero de 2008 sustituyendo al Boletín D-3 “Obligaciones Laborales”.</p>
+                        <p>Los Pasivos Laborales generados por los empleados de una Empresa  deben ser cuantificados actuarialmente y revelados conforme a los Principios de Contabilidad Generalmente Aceptados.</p>
+                        <p>La Prima de Antigüedad y Las Remuneraciones al Término de la Relación Laboral (Indemnización Legal), son ejemplos de ese tipo de pasivos cuyo comportamiento es normalmente creciente.</p>
+                        <div>
+                            <p>La finalidad de la NIF D-3 consiste en:</p>
+                            <ul>
+                                <li>Convergencia con Normas Internacionales de Información Financiera (NIIF).</li>
+                                <li>Integración del Boletín D-4 (Tratamiento Contable I.S.R. y P.T.U.).</li>
+                                <li>Inclusión de Párrafos Boletín C-9 (Beneficios de Terminación por Reestructura).</li>
+                                <li>Tratamiento homogéneo con la NIF B-10.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2>Objetivo</h2>
+                    <p>Establecer Normas para el Reconocimiento Contable de los Beneficios que la entidad otorga a sus empleados y otras provisiones relativas.</p>
+                </div>
+                <div>
+                    <h2>Alcance</h2>
+                    <div>
+                        <p>La aplicación por parte de Entidades que emiten Estados Financieros conforme a NIF A-3.</p>
+                        <div>
+                            <p>Se deberá reconocer un Pasivo por Beneficios a empleados si:</p>
+                            <ul>
+                                <li>Existe obligación (legal o implícita) de efectuar pagos en el futuro por sucesos del pasado.</li>
+                                <li>La obligación es atribuible a servicios prestados.</li>
+                                <li>La obligación está devengada.</li>
+                                <li>Es probable el pago del Beneficio.</li>
+                                <li>El monto puede ser cuantificable.</li>
+                                <li>Si no se cumplen todas las condiciones se debe estimar la posible contingencia acumulada y revelarla.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2>Vigencia</h2>
+                    <div>
+                        <p>Las disposiciones contenidas en la NIF D-3 entran en vigor para ejercicios que inicien a partir del 1de enero de 2008.</p>
+                        <p>Se dejan sin efecto los Boletines D-3 "Obligaciones Laborales", D-4 "Tratamiento Contable del ISR, del impuesto al activo y de la PTU", así como la INIF-4 "Presentación en el estado de resultados de la PTU".</p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
--- client/src/pages/usgaap/Usgaap.jsx
+++ client/src/pages/usgaap/Usgaap.jsx
@@ -4,7 +4,41 @@ import "./Usgaap.css";
 const Usgaap = () => {
     return (
         <section>
-
+            <h1>USGAAP</h1>
+            <p>En los Estados Unidos de América la entidad encargada de elaborar los ordenamientos de índole contable es el F.A.S.B. (Financial Accounting Standards Board). Entre los boletines (Statements) que este organismo ha emitido, existen cuatro en particular que requieren la participación de un actuario consultor para su elaboración y aplicación.</p>
+            <div>
+                <p>Estos boletines son los siguientes:</p>
+                <ul>
+                    <li>
+                        <h2>SFAS-87 (Employer´s Accounting for Pensions)</h2>
+                        <div>
+                            <p>Tiene como alcance la determinación de obligaciones laborales y aplicación contable generadas por los Planes de Pensiones, Planes de Retiro, Beneficios por Antigüedad y similares, que las empresas otorgan a sus trabajadores (La NIF D-3 es su equivalente).</p>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>SFAS-88 (Curtailments and Termination Benefits)</h2>
+                        <div>
+                            <p>Este es un boletín complementario al SFAS-87, ya que tiene por objeto la valuación y registro contable de eventos particulares que pueden presentarse en una empresa que cuente con un plan de pensiones o similar:</p>
+                            <ul>
+                                <li>La terminación definitiva del Plan (Liquidación de la empresa, venta, fusión, escisión, etc.)</li>
+                                <li>Las liquidaciones parciales de pasivos (Pagos únicos, Ventas parciales, Traspaso de personal entre empresas con reconocimiento de antigüedad, etc.)</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>SFAS-106 (Post-Retirement Benefits)</h2>
+                        <div>
+                            <p>Este boletín tiene como objetivo la valuación y contabilización de los beneficios diferentes a pensiones que la empresa otorga a quienes se retiran de la empresa, como por ejemplo los gastos médicos a jubilados.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>SFAS-112 (Post-Employment Benefits)</h2>
+                        <div>
+                            <p>Comprende el registro contable del monto de los pasivos generados por los beneficios contingentes diferentes al sueldo pagado a los empleados durante su vida laboral en la empresa hasta que se retiran, como por ejemplo: La extensión de las prestaciones de la empresa a trabajadores que dejen de laborar de manera temporal.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </section>
     );
 };
