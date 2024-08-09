11e396c313de8a1eb43bb97d767cc778971539a5
Polar <cristianga2002@ciencias.unam.mx>
2024-08-09 12:25:54 -0600

    Agregando esqueleto de la vista de prevision social

--- client/src/pages/social-welfare/SocialWelfare.jsx
+++ client/src/pages/social-welfare/SocialWelfare.jsx
@@ -4,7 +4,23 @@ import "./SocialWelfare.css";
 const SocialWelfare = () => {
     return (
         <section>
-
+            <h1>Previsión Social</h1>
+            <div>
+                <p>Mediante un programa de Previsión Social se pretende la optimización del esquema de retribución a los empleados, aprovechando la implantación de prestaciones de esta índole en el contexto laboral, o bien, modificando las ya existentes, aprovechando siempre las ventajas fiscales aplicables.</p>
+                <p>Los beneficios que se proporcionan mediante un esquema de esta naturaleza, son adicionales a los otorgados por las instituciones públicas de Seguridad Social en los casos de enfermedades generales, maternidad, invalidez, vejez, cesantía en edad avanzada y muerte.</p>
+                <p>La flexibilidad de este tipo de planes permite que el empleado adapte a sus necesidades particulares las prestaciones a las que tiene derecho.</p>
+                <p>El programa es además, un instrumento adecuado para afrontar períodos de economía inflacionaria además de ser un medio para mejorar la productividad del personal.</p>
+                <p>La gama de beneficios que se pueden otorgar bajo este concepto son, entre otros:</p>
+                <ul>
+                    <li>Guarderías.</li>
+                    <li>Actividades deportivas</li>
+                    <li>Actividades culturales</li>
+                    <li>Becas educacionales</li>
+                    <li>Servicios médicos y hospitalarios</li>
+                    <li>Seguros de vida</li>
+                    <li>Seguros de gastos médicos</li>
+                </ul>
+            </div>
         </section>
     );
 };
