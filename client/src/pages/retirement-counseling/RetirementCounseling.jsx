eca7fa3c1ecbc9cdd682636675e6e3bf8de8c652
Polar <cristianga2002@ciencias.unam.mx>
2024-08-09 12:34:05 -0600

    Agregando esqueleto de la vista de asesoria para el retiro sin estilos

--- client/src/pages/retirement-counseling/RetirementCounseling.jsx
+++ client/src/pages/retirement-counseling/RetirementCounseling.jsx
@@ -4,7 +4,40 @@ import "./RetirementCounseling.css";
 const RetirementCounseling = () => {
     return (
         <section>
-
+            <h1>Asesoría para el Retiro</h1>
+            <div>
+                <p>Brindamos asesoría profesional, individual y confidencial para la conformación del plan personal de retiro:</p>
+                <ul>
+                    <li>Pensión IMSS</li>
+                    <li>Afore</li>
+                    <li>Planes personales de Retiro</li>
+                    <li>Asesoría Infonavit</li>
+                </ul>
+            </div>
+            <div>
+                <p>Le ayudaremos a incrementar de forma importante el monto de sus recursos invertidos en su Afore.</p>
+            </div>
+            <div>
+                <p>Le ayudaremos a entender qué hacer para jubilarse por la Ley 73, y establecer una estrategia para mejorar el monto de su pensión:</p>
+                <ul>
+                    <li>Goce de una pensión mensual vitalicia a partir de los 60 años.</li>
+                    <li>Mejore sustancialmente el monto de su pensión mensual</li>
+                    <li>Reciba en una sola exhibición la mayor cantidad de los recursos que tenga en su AFORE y en el INFONAVIT</li>
+                </ul>
+            </div>
+            <div>
+                <p>Si usted es un trabajador que cotizó al IMSS antes del 1 de julio de 1997 contáctenos ESTÉ O NO actualmente cotizando.</p>
+                <p>Le ayudaremos a ahorrar a través de los instrumentos que mejor se adapten a sus necesidades y perfil de inversión. El ahorro es fundamental para la creación de riqueza. Construya su patrimonio, y dé sentido a su vida ahorrando de forma constante, con metas claras a corto, mediano y largo plazo.</p>
+            </div>
+            <div>
+                <p>Al realizar aportaciones voluntarias a su Afore, podrá deducirlas de impuestos, además de otorgarle excelentes rendimientos. Le ayudamos a analizar qué es lo que más le conviene, en qué fondo de la Afore conviene más invertirlo y, lo compararemos con otros fondos de inversión que manejamos:</p>
+                <ul>
+                    <li>Fondos del Mercado de Capitales (indexados al índice de la Bolsa)</li>
+                    <li>Fondos de Deuda para Personas Físicas</li>
+                    <li>Fondos de Deuda para Personas Morales</li>
+                    <li>Fondos de Jubilación (deducibles de impuestos)</li>
+                </ul>
+            </div>
         </section>
     );
 };
