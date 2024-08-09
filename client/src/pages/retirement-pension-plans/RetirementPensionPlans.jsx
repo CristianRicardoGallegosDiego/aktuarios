4657d6d2011efc75c1e3a6818f6968bfe5e3ed1f
Polar <cristianga2002@ciencias.unam.mx>
2024-08-09 12:21:49 -0600

    Agregando esqueleto de la vista de planes de pensiones por jubilacion sin estilos

--- client/src/pages/retirement-pension-plans/RetirementPensionPlans.jsx
+++ client/src/pages/retirement-pension-plans/RetirementPensionPlans.jsx
@@ -4,7 +4,13 @@ import "./RetirementPensionPlans.css";
 const RetirementPensionPlans = () => {
     return (
         <section>
-
+            <h1>Planes de Pensiones por Jubilación</h1>
+            <div>
+                <p>Mediante la implantación de un plan de pensiones por jubilación, se pretende otorgar a los empleados de la empresa un beneficio que les permita llegar a la edad de retiro con la seguridad de obtener un ingreso, ya sea en forma mensual o de pago único, que sumado a los beneficios que por este concepto conceden las diferentes instituciones de seguridad social, les resulte suficiente para afrontar su futuro económico.</p>
+                <p>Por otra parte, el constituir una reserva para jubilaciones significa reconocer financieramente el envejecimiento que van sufriendo los empleados y permite amortizar, en el transcurso de la vida activa del personal, el pasivo que genera al acumular antigüedad dentro de la empresa.</p>
+                <p>La labor del Actuario Consultor en este ámbito, consiste en el diseño del plan de pensiones más adecuado al entorno y necesidades de la empresa, la valuación actuarial en la cual se determinan los pasivos de la empresa y la forma de financiarlos, la administración diaria del plan, su implantación ante autoridades y su comunicación al personal.</p>
+                <p>La labor del Actuario Consultor en este ámbito, consiste en el diseño del plan de pensiones más adecuado al entorno y necesidades de la empresa, la valuación actuarial en la cual se determinan los pasivos de la empresa y la forma de financiarlos, la administración diaria del plan, su implantación ante autoridades y su comunicación al personal.</p>
+            </div>
         </section>
     );
 };
