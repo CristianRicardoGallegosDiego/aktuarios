e44a5651a017e5c6a53a0eeb7e2c4ad18c78e297
Polar <cristianga2002@ciencias.unam.mx>
2024-09-28 16:54:57 -0600

    Vista de clientes terminada con disenno responsivo

--- client/src/pages/clients/Clients.jsx
+++ client/src/pages/clients/Clients.jsx
@@ -5,10 +5,11 @@ import "./Clients.css";
 const Clients = () => {
     return (
         <section className="clients-container">
-            <h1>¿Por qué confiar en nuestra consultoría Aktuarios?</h1>
-            <h2>Respaldada por expertos.</h2>
-            <h2>Respaldada por expertos.</h2>
-            <p>Con experiencia y precisión, ayudamos a las empresas a reconocer y gestionar sus pasivos contingentes, asegurando decisiones organizacionales acertadas que impulsen la rentabilidad. Nuestro equipo de expertos está comprometido en ofrecer soluciones actuariales, financieras y contables adaptadas a tus necesidades, para que cada decisión sume valor.</p>
+            <div className="information-container">
+                <h1>¿Por qué confiar en nuestra consultoría Aktuarios?</h1>
+                <h2>Respaldada por expertos.</h2>
+                <p>Con experiencia y precisión, ayudamos a las empresas a reconocer y gestionar sus pasivos contingentes, asegurando decisiones organizacionales acertadas que impulsen la rentabilidad. Nuestro equipo de expertos está comprometido en ofrecer soluciones actuariales, financieras y contables adaptadas a tus necesidades, para que cada decisión sume valor.</p>
+            </div>
             <div className="slider-container">
                 <div className="slide-track">
                     {
