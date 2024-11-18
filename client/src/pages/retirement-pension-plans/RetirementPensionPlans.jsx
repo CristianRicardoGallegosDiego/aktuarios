719995acbcfc8ba33497a801aa5a9c3502b81a90
Polar <cristianga2002@ciencias.unam.mx>
2024-11-17 20:47:54 -0600

    terminando vista de planes de pensiones por jubilacion para disenno de pc

--- client/src/pages/retirement-pension-plans/RetirementPensionPlans.jsx
+++ client/src/pages/retirement-pension-plans/RetirementPensionPlans.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import Image1 from "../../assets/images/retirement-pension-plans/image1.png";
+import Image2 from "../../assets/images/retirement-pension-plans/image2.png";
 import "./RetirementPensionPlans.css";
 
 const RetirementPensionPlans = () => {
     return (
         <section className="retirement-pension-plans">
-            <h1>Planes de Pensiones por Jubilación</h1>
-            <div>
-                <p>Mediante la implantación de un plan de pensiones por jubilación, se pretende otorgar a los empleados de la empresa un beneficio que les permita llegar a la edad de retiro con la seguridad de obtener un ingreso, ya sea en forma mensual o de pago único, que sumado a los beneficios que por este concepto conceden las diferentes instituciones de seguridad social, les resulte suficiente para afrontar su futuro económico.</p>
-                <p>Por otra parte, el constituir una reserva para jubilaciones significa reconocer financieramente el envejecimiento que van sufriendo los empleados y permite amortizar, en el transcurso de la vida activa del personal, el pasivo que genera al acumular antigüedad dentro de la empresa.</p>
-                <p>La labor del Actuario Consultor en este ámbito, consiste en el diseño del plan de pensiones más adecuado al entorno y necesidades de la empresa, la valuación actuarial en la cual se determinan los pasivos de la empresa y la forma de financiarlos, la administración diaria del plan, su implantación ante autoridades y su comunicación al personal.</p>
-                <p>La labor del Actuario Consultor en este ámbito, consiste en el diseño del plan de pensiones más adecuado al entorno y necesidades de la empresa, la valuación actuarial en la cual se determinan los pasivos de la empresa y la forma de financiarlos, la administración diaria del plan, su implantación ante autoridades y su comunicación al personal.</p>
+            <div className="banner">
+                <h1>Planes de Pensiones por Jubilación</h1>
+                <div></div>
             </div>
+            <section className="information-container">
+                <div className="items">
+                    <div className="ps">
+                        <p>La implantación de un plan de pensiones busca asegurar a los empleados un ingreso al llegar a la jubilación, complementando los beneficios de las instituciones de seguridad social.</p>
+                        <p>Constituir una reserva para jubilaciones reconoce el envejecimiento de los empleados y permite amortizar el pasivo generado por su antigüedad en la empresa.</p>
+                    </div>
+                    <div className="img">
+                        <img src={Image1} alt="Imagen de la empresa."/>
+                    </div>
+                </div>
+                <div className="items">
+                    <div className="img">
+                        <img src={Image2} alt="Imagen de la empresa."/>
+                    </div>
+                    <div className="ps">
+                        <p>La labor del Actuario Consultor incluye diseñar el plan de pensiones adecuado, valorar los pasivos y su financiamiento, administrar el plan, implementarlo ante autoridades y comunicarlo al personal.</p>
+                        <p>La labor del Actuario Consultor incluye diseñar el plan de pensiones adecuado, valorar pasivos y su financiamiento, administrar el plan, implementarlo y comunicarlo al personal.</p>
+                    </div>
+                </div>
+            </section>
         </section>
     );
 };
