93de4b8f5e417166c723cdc6a011fde874df0005
Polar <cristianga2002@ciencias.unam.mx>
2024-12-30 17:40:26 -0600

    Formulario semi terminado sin expresiones regex y sin verificaciones

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import {
     Route 
 } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
+import { 
+  SnackbarProvider,
+} from "notistack";
 import ScrollToTop from "./hooks/ScrollToTop";
 import MainHeader from "./components/sections/headers/main-header/MainHeader.jsx";
 import Footer from "./components/sections/footer/Footer.jsx";
@@ -27,27 +30,37 @@ import "./assets/styles/App.css";
 
 const App = () => {
   return (
-    <NextUIProvider>
-      <Router>
-        <ScrollToTop />
-        <MainHeader />
-        <Routes>
-          <Route path="/"                                                   element={<Home />} />
-          <Route path="/nosotros"                                           element={<AboutUs />} />
-          <Route path="/servicios"                                          element={<OurServices />} />
-          <Route path="/servicios/pasivos-contingentes-laborales/nif-d3"    element={<NifD3 />} />
-          <Route path="/servicios/pasivos-contingentes-laborales/usgaap"    element={<Usgaap />} />
-          <Route path="/servicios/pasivos-contingentes-laborales/ias"       element={<IAS />} />
-          <Route path="/servicios/valuacion-actuarial-y-revision-de-los-pasivos-laborales"          element={<ActuarialValuationAndReviewOfLaborLiabilities />} />
-          <Route path="/servicios/plan-de-retiro"                         element={<RetirementPlan />} />
-          <Route path="/servicios/otros-servicios"                  element={<OtherServices />} />
-          <Route path="/clientes"                                           element={<Clients />} />
-          <Route path="/contactanos"                                        element={<ContactUs />} />
-          <Route path="*"                                                   element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </NextUIProvider>
+    <SnackbarProvider
+      maxSnack={1}
+      autoHideDuration={5000}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      
+    >
+      <NextUIProvider>
+        <Router>
+          <ScrollToTop />
+          <MainHeader />
+          <Routes>
+            <Route path="/"                                                   element={<Home />} />
+            <Route path="/nosotros"                                           element={<AboutUs />} />
+            <Route path="/servicios"                                          element={<OurServices />} />
+            <Route path="/servicios/pasivos-contingentes-laborales/nif-d3"    element={<NifD3 />} />
+            <Route path="/servicios/pasivos-contingentes-laborales/usgaap"    element={<Usgaap />} />
+            <Route path="/servicios/pasivos-contingentes-laborales/ias"       element={<IAS />} />
+            <Route path="/servicios/valuacion-actuarial-y-revision-de-los-pasivos-laborales"          element={<ActuarialValuationAndReviewOfLaborLiabilities />} />
+            <Route path="/servicios/plan-de-retiro"                         element={<RetirementPlan />} />
+            <Route path="/servicios/otros-servicios"                  element={<OtherServices />} />
+            <Route path="/clientes"                                           element={<Clients />} />
+            <Route path="/contactanos"                                        element={<ContactUs />} />
+            <Route path="*"                                                   element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </NextUIProvider>
+    </SnackbarProvider>
   );
 };
 
--- client/src/pages/contact-us/ContactUs.jsx
+++ client/src/pages/contact-us/ContactUs.jsx
@@ -6,8 +6,6 @@ import { sendEmail } from "../../services/email";
 //import StaticTruck from "../../assets/images/Truck.svg";
 //import DynamicTruck from "../../assets/images/Truck.gif";
 import "./ContactUs.css";
-
-
 import { Input } from "@nextui-org/react";
 import { Textarea } from "@nextui-org/react";
 import {
@@ -18,15 +16,36 @@ import {
     ModalFooter,
 } from "@nextui-org/modal";
 import { Button } from "@nextui-org/react";
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { enqueueSnackbar } from "notistack";
 
-const ContactUsForm = () => {
+function GradientCircularProgress() {
+    return (
+        <React.Fragment>
+            <svg width={0} height={0}>
+                <defs>
+                    <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <stop offset="0%" stopColor="#e01cd5" />
+                        <stop offset="100%" stopColor="#1CB5E0" />
+                    </linearGradient>
+                </defs>
+            </svg>
+            <CircularProgress sx={{ 'svg circle': { stroke: 'url(#my_gradient)' } }} size="3rem" />
+        </React.Fragment>
+    );
+}
+
+const ContactUs = () => {
     const [fullName, setFullName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [subject, setSubject] = useState("");
     const [message, setMessage] = useState("");
-
     const [isOpen, setIsOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+
 
     const handleOpen = (e) => {
         e.preventDefault();
@@ -36,6 +55,7 @@ const ContactUsForm = () => {
     const handleEmail = async (e) => {
         e.preventDefault();
         setIsOpen(false);
+        setIsLoading(true);
 
         const formData = {
             fullname: fullName,
@@ -46,134 +66,136 @@ const ContactUsForm = () => {
         };
 
         try {
-            const result = await sendEmail(formData);
-            alert(result.message);
+            await sendEmail(formData);
+            enqueueSnackbar("¡Mensaje enviado con éxito :)!", { variant: "success" });
+            setFullName("");
+            setEmail("");
+            setPhone("");
+            setSubject("");
+            setMessage("");
         } catch (error) {
-            alert(error.message);
+            enqueueSnackbar("¡Error! No se pudo enviar el mensaje :(", { variant: "error" });
         } finally {
-            
+            setIsLoading(false);
         }
     };
-    
-    
-    
 
-    return (
-        <section className="contact-us-form-container">
-            <h1>¡Nos encanta escuchar!</h1>
-            <p>
-                Gracias por dedicar su valioso tiempo a interactuar con nosotros. Queremos que se sienta comprendido
-                y apreciado en cada paso del camino. Inicie el diálogo y responderemos lo antes posible con el
-                objetivo de ofrecerle el mejor servicio de acuerdo a sus necesidades.
-            </p>
-            <form className="form-container">
-                <Input 
-                    id="name"
-                    type="text"
-                    label="Nombre Completo"
-                    errorMessage="¿Así se escribe? Inténtalo otra vez."
-                    isClearable
-                    variant="bordered"
-                    size="md"
-                    color="default"
-                    className="my-input"
-                    onChange={(e) => setFullName(e.target.value)}
-                />
-                <div className="email-and-phone">
-                    <Input 
-                        id="email"
-                        type="email"
-                        label="Correo Electrónico"
-                        errorMessage="¡Oops! Ese correo no tiene buena pinta."
-                        isClearable
-                        variant="bordered"
-                        size="md"
-                        color="default"
-                        className="my-input"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <Input 
-                        id="phone"
-                        type="text"
-                        label="Teléfono"
-                        errorMessage="¡Error! Ese número no suena en nuestro radar."
-                        isClearable
-                        variant="bordered"
-                        size="md"
-                        color="default"
-                        className="my-input"
-                        onChange={(e) => setPhone(e.target.value)}
-                    />
-                </div>
-                <Input 
-                    id="subject"
-                    type="text"
-                    label="Asunto"
-                    errorMessage="¡Vaya! Parece que olvidaste ingresar el asunto."
-                    isClearable
-                    variant="bordered"
-                    size="md"
-                    color="default"
-                    className="my-input"
-                    onChange={(e) => setSubject(e.target.value)}
-                />
-                <Textarea
-                    id="message"
-                    type="text"
-                    label="Mensaje"
-                    errorMessage="¡Vaya! Tu mensaje está haciendo acrobacias invisibles. Inténtalo de nuevo."
-                    minRows={5}
-                    maxRows={5}
-                    variant="bordered"
-                    size="md"
-                    color="default"
-                    className="my-input"
-                    onChange={(e) => setMessage(e.target.value)}
-                />
-                <button onClick={handleOpen}>
-                    <p>Enviar</p>
-                </button>
-            </form>
-            <Modal 
-                isOpen={isOpen}
-                onClose={() => setIsOpen(false)}
-                isDismissable={false}
-                backdrop="blur"
-            >
-                <ModalContent>
-                    <ModalHeader className="flex flex-col gap-1">¡Misión Mensaje!</ModalHeader>
-                    <ModalBody>
-                        <p>¡El reloj avanza! ¿Confirmas que deseas enviar este mensaje para una evaluación experta?</p>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button
-                            onClick={() => setIsOpen(false)}
-                            color="danger"
-                            variant="light"
-                        >
-                            ¡Fin del viaje!
-                        </Button>
-                        <Button
-                            color="primary"
-                            className="text-white font-bold"
-                            onClick={handleEmail}
-                        >
-                            ¡Lanzar cohete!
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </section>
-    );
-};
-
-const ContactUs = () => {
     return (
         <section className="contact-us-page-container">
             <div className="contact-us-globe-container">
                 <GlobeView />
             </div>
-            <ContactUsForm />
+            <section className="contact-us-form-container">
+                <h1>¡Nos encanta escuchar!</h1>
+                <p>
+                    Gracias por dedicar su valioso tiempo a interactuar con nosotros. Queremos que se sienta comprendido
+                    y apreciado en cada paso del camino. Inicie el diálogo y responderemos lo antes posible con el
+                    objetivo de ofrecerle el mejor servicio de acuerdo a sus necesidades.
+                </p>
+                <form className="form-container">
+                    <Input 
+                        id="name"
+                        type="text"
+                        label="Nombre Completo"
+                        errorMessage="¿Así se escribe? Inténtalo otra vez."
+                        isClearable
+                        variant="bordered"
+                        size="md"
+                        color="default"
+                        className="my-input"
+                        onChange={(e) => setFullName(e.target.value)}
+                    />
+                    <div className="email-and-phone">
+                        <Input 
+                            id="email"
+                            type="email"
+                            label="Correo Electrónico"
+                            errorMessage="¡Oops! Ese correo no tiene buena pinta."
+                            isClearable
+                            variant="bordered"
+                            size="md"
+                            color="default"
+                            className="my-input"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Input 
+                            id="phone"
+                            type="text"
+                            label="Teléfono"
+                            errorMessage="¡Error! Ese número no suena en nuestro radar."
+                            isClearable
+                            variant="bordered"
+                            size="md"
+                            color="default"
+                            className="my-input"
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </div>
+                    <Input 
+                        id="subject"
+                        type="text"
+                        label="Asunto"
+                        errorMessage="¡Vaya! Parece que olvidaste ingresar el asunto."
+                        isClearable
+                        variant="bordered"
+                        size="md"
+                        color="default"
+                        className="my-input"
+                        onChange={(e) => setSubject(e.target.value)}
+                    />
+                    <Textarea
+                        id="message"
+                        type="text"
+                        label="Mensaje"
+                        errorMessage="¡Vaya! Tu mensaje está haciendo acrobacias invisibles. Inténtalo de nuevo."
+                        minRows={5}
+                        maxRows={5}
+                        variant="bordered"
+                        size="md"
+                        color="default"
+                        className="my-input"
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <button onClick={handleOpen}>
+                        <p>Enviar</p>
+                    </button>
+                </form>
+                <Modal 
+                    isOpen={isOpen}
+                    onClose={() => setIsOpen(false)}
+                    isDismissable={false}
+                    backdrop="blur"
+                >
+                    <ModalContent>
+                        <ModalHeader className="flex flex-col gap-1">¡Misión Mensaje!</ModalHeader>
+                        <ModalBody>
+                            <p>¡El reloj avanza! ¿Confirmas que deseas enviar este mensaje para una evaluación experta?</p>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button
+                                onClick={() => setIsOpen(false)}
+                                color="danger"
+                                variant="light"
+                            >
+                                ¡Fin del viaje!
+                            </Button>
+                            <Button
+                                color="primary"
+                                className="text-white font-bold"
+                                onClick={handleEmail}
+                            >
+                                ¡Lanzar cohete!
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </section>
+            <Backdrop
+                sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 100 })}
+                open={isLoading}
+            >
+                <GradientCircularProgress />
+            </Backdrop>
         </section>
     );
 };
